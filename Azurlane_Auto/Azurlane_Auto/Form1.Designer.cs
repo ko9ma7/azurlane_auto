de1f288de2b595306bc22bdbc915500d2f329bd7
kurokuma <sirokuma427@gmail.com>
2018-03-15 10:24:44 +0900

    gui
    gui

---
+++ Azurlane_Auto/Azurlane_Auto/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Azurlane_Auto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.file = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.actionLabel = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.encounterBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.file.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(12, 30);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(149, 58);
+            this.start.TabIndex = 0;
+            this.start.Text = "開始";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.stop.Location = new System.Drawing.Point(252, 30);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(149, 61);
+            this.stop.TabIndex = 2;
+            this.stop.Text = "停止";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // file
+            // 
+            this.file.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem});
+            this.file.Location = new System.Drawing.Point(0, 0);
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(413, 24);
+            this.file.TabIndex = 3;
+            this.file.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // logToolStripMenuItem
+            // 
+            this.logToolStripMenuItem.Checked = true;
+            this.logToolStripMenuItem.CheckOnClick = true;
+            this.logToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.logToolStripMenuItem.Name = "logToolStripMenuItem";
+            this.logToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.logToolStripMenuItem.Text = "ログ";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(95, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "終了";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.actionLabel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 105);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 65);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "行動";
+            // 
+            // actionLabel
+            // 
+            this.actionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.actionLabel.AutoSize = true;
+            this.actionLabel.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.actionLabel.Location = new System.Drawing.Point(24, 26);
+            this.actionLabel.Name = "actionLabel";
+            this.actionLabel.Size = new System.Drawing.Size(59, 29);
+            this.actionLabel.TabIndex = 0;
+            this.actionLabel.Text = "なし";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.logBox);
+            this.groupBox3.Location = new System.Drawing.Point(12, 176);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(389, 159);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "ログ";
+            // 
+            // logBox
+            // 
+            this.logBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logBox.HideSelection = false;
+            this.logBox.Location = new System.Drawing.Point(7, 19);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logBox.Size = new System.Drawing.Size(376, 134);
+            this.logBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.encounterBox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(309, 105);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(92, 65);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "遭遇数";
+            // 
+            // encounterBox
+            // 
+            this.encounterBox.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.encounterBox.Location = new System.Drawing.Point(6, 19);
+            this.encounterBox.Name = "encounterBox";
+            this.encounterBox.Size = new System.Drawing.Size(49, 36);
+            this.encounterBox.TabIndex = 0;
+            this.encounterBox.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(50, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "回";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 356);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.file);
+            this.MainMenuStrip = this.file;
+            this.Name = "Form1";
+            this.Text = "アズールレーン自動化";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.file.ResumeLayout(false);
+            this.file.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.MenuStrip file;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label actionLabel;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox encounterBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Azurlane_Auto/Azurlane_Auto/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Configuration;
+
+namespace Azurlane_Auto
+{
+    public partial class Form1 : Form
+    {
+        private bool is_run = false;
+        private string[] template_files;
+        Window window = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void exit()
+        {
+            Application.Exit();
+        }
+
+        private string get_emu_name()
+        {
+            string[] emus = new string[] { "Nox", "BlueStack3" };
+            System.Diagnostics.Process[] ps = System.Diagnostics.Process.GetProcesses();
+            foreach (System.Diagnostics.Process p in ps)
+            {
+                if (0 <= Array.IndexOf(emus, p.ProcessName))
+                {
+                    return p.ProcessName;
+                }
+            }
+            return null;
+        }
+
+        private string read_ss_file()
+        {
+            try
+            {
+                return string.Format("{0}\\{1}\\{2}", Directory.GetCurrentDirectory(), read_config("ss_folder"), read_config("ss_file_name"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        private string[] read_template_files()
+        {
+            try
+            {
+                return Directory.GetFiles(
+                    Directory.GetCurrentDirectory() + "\\" + read_config("template_folder"),
+                    "*",
+                    SearchOption.AllDirectories
+                    );
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        private string read_config(string str)
+        {
+            try
+            {
+                return ConfigurationSettings.AppSettings[str].ToString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        private bool exist_ss_folder()
+        {
+            if (Directory.Exists(read_config("ss_folder")))
+            {
+                return true;
+            }
+            else
+            {
+                Directory.CreateDirectory(
+                    Directory.GetCurrentDirectory() + "\\" + read_config("ss_folder")
+                    );
+                return false;
+            }
+        }
+
+        private void capture()
+        {
+            Bitmap resize_img(Bitmap image, double width, double height)
+            {
+                double hi;
+                double imagew = image.Width;
+                double imageh = image.Height;
+
+                if ((height / width) <= (imageh / imagew))
+                {
+                    hi = height / imageh;
+                }
+                else
+                {
+                    hi = width / imagew;
+                }
+                int w = (int)(imagew * hi);
+                int h = (int)(imageh * hi);
+
+                Bitmap result = new Bitmap(w, h);
+                Graphics g = Graphics.FromImage(result);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(image, 0, 0, result.Width, result.Height);
+
+                return result;
+            }
+            resize_img(window.CaptureImage(), 832, 498).Save(Directory.GetCurrentDirectory() + "\\" + read_config("ss_folder") + "\\" + read_config("ss_file_name"));
+        }
+
+        private void delete_ss()
+        {
+            //あとで
+        }
+
+        private string now()
+        {
+            return null;
+        }
+
+        private void add_log(string type, string str)
+        {
+            logBox.AppendText("[" + type + "] " + str + "\n");
+        }
+
+        private void add_encounter(int i)
+        {
+            encounterBox.Clear();
+            encounterBox.AppendText(i.ToString());
+        }
+
+        private void set_action_label(string str)
+        {
+            actionLabel.ResetText();
+            actionLabel.Text = str;
+        }
+
+        /*
+         自動生成
+         */
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            add_log("+", "プログラム起動");
+            var emu_name = get_emu_name();
+            if (emu_name != null)
+            {
+                window = new Window(emu_name);
+                add_log("+", emu_name + " の起動確認");
+
+                template_files = read_template_files();
+                if (template_files != null)
+                {
+                    add_log("+", "テンプレートファイル読み込み");
+                    if (exist_ss_folder())
+                    {
+                        add_log("+", "スクリーンショットフォルダ確認");
+                    }
+                    else
+                    {
+                        add_log("x", "スクリーンショットフォルダを生成");
+                    }
+                }
+                else
+                {
+                    add_log("x", "テンプレートファイルが読み込めません");
+                }
+                
+            }
+            else
+            {
+                add_log("x", "エミュレータが起動してません");
+            }
+        }
+
+        private async void start_Click(object sender, EventArgs e)
+        {
+            //inner func
+            List<(int, int)> get_all_pos(Mat result, double match_rate = 0.3)
+            {
+                var t = new List<int>();
+                var pos_list = new List<(int, int)>();
+
+                for (int y = 0; y < result.Rows; y++)
+                {
+                    for (int x = 0; x < result.Cols; x++)
+                    {
+                        if (result.At<float>(y, x) > 0)
+                        {
+                            int d = (int)Math.Round(Math.Round(Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) / 10, 0, MidpointRounding.AwayFromZero) / 10, 1, MidpointRounding.AwayFromZero);
+                            if (t.Exists(s => s == d))
+                            {
+                                //pass
+                            }
+                            else
+                            {
+                                pos_list.Add((x, y));
+                                t.Add(d);
+                            }
+                        }
+                    }
+                }
+                return pos_list;
+            }
+
+            //inner func
+            List<((int, int), string)> template_matching()
+            {
+                Mat ss_img = new Mat(read_ss_file());
+                var res = new List<((int, int), string)>();
+                foreach (string file_path in read_template_files())
+                {
+                    Mat tmplate = new Mat(file_path);
+                    double min_val, max_val;
+                    OpenCvSharp.Point min_loc, max_loc;
+                    string match_type = Path.GetFileNameWithoutExtension(file_path);
+                    var result = new Mat();
+                    Cv2.MatchTemplate(ss_img, tmplate, result, TemplateMatchModes.CCoeffNormed);
+                    Cv2.MinMaxLoc(result, out min_val, out max_val, out min_loc, out max_loc, null);
+
+                    if (max_val < 0.5) continue;
+
+                    Cv2.Threshold(result, result, 0.4, 1.0, ThresholdTypes.Tozero);
+                    List<(int, int)> pos_list = get_all_pos(result);
+                    foreach ((int, int) pos in pos_list)
+                    {
+                        res.Add((pos, match_type));
+                    }
+                }
+                return res;
+            }
+
+            void select_action()
+            {
+                //
+            }
+
+            //start start_Click
+            if (!is_run)
+            {
+                is_run = true;
+                add_log("+", "開始");
+
+                while (is_run)
+                {
+                    add_log("*", "スクリーンキャプチャ");
+                    //capture();
+
+                    foreach(var s in template_matching())
+                    {
+                        Console.WriteLine(s);
+                    }
+                    
+                    await Task.Delay(1000);
+                    break;
+                }
+            }
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            if (is_run)
+            {
+                is_run = false;
+                add_log("+", "停止");
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            exit();
+        }
+
+        /*
+         ここまで
+        */
+    }
+}
